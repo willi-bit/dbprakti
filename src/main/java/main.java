13c7da867047f0ff05353cdf1ddd28408eafce4e
Willi <62183767+willi-bit@users.noreply.github.com>
2024-05-30 19:43:40 +0200

    first iteration of parsing

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,11 @@
 public class main {
     public static void main(String[] args) {
-
+        System.out.println("STARTING");
+        try {
+            //importData.importDataFromFile("data/categories.xml");
+            XMLToDatabase xmlToDatabase = new XMLToDatabase();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
